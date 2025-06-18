7f33eff161248de3b5e3c4d04d18bd26e0011ddb
Enrique Muñoz <enriquemunozdev@gmail.com>
2025-06-18 16:12:20 -0600

    configured status bar for only android devices

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,16 @@
 import { globalStyles } from "@/styles/global-styles";
 import { useFonts } from "expo-font";
+import * as NavigationBar from "expo-navigation-bar";
 import { Slot } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { View } from "react-native";
+import { Platform, View } from "react-native";
+
+const isAndroid = Platform.OS === "android";
+
+if (isAndroid) {
+  NavigationBar.setBackgroundColorAsync("black");
+}
 
 const RootLayout = () => {
   const [loaded] = useFonts({
